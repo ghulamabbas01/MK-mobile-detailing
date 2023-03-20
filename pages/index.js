dac61b5b4c59cabb75a2eaf915015e97be915b8f
Abbas Khan <ak6119231@gmail.com>
2023-03-20 20:05:04 +0500

    push

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import * as React from "react";
 import { Box } from "@mui/material";
 import NavigationBar from "../src/layout/navigationBar";
 import Detaling from "../src/components/Detaling";
+import Hero from "../src/components/Hero";
 
 export default function Index() {
   return (
     <Box sx={{ bgcolor: "#000" }}>
       <NavigationBar />
+      <Hero />
       <Detaling />
     </Box>
   );
